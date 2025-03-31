dee44073da35ac98ad354568bcfe2a894551383c
Stanxnz <stanvdkooi@gmail.com>
2025-03-31 23:57:09 +0200

    made a functioning local storage system

--- app.mjs
+++ app.mjs
@@ -1,1 +1,25 @@
 console.log("APP");
+
+const todoForm = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
+
+let thingsYouNeedToDo = [];
+
+document.addEventListener("StorageLoaded"), () => {
+    const savedThingsYouNeedToDo = localStorage.getItem("Things to do");
+    if (savedThingsYouNeedToDo){
+        thingsYouNeedToDo = JSON.parse(savedThingsYouNeedToDo);
+        thingsYouNeedToDo.forEach((thingsYouNeedToDo) => addThingsYouNeedToDoToStorage(thingsYouNeedToDo));
+            
+     }
+    };
+
+function saveThingsYouNeedToDo() {
+    localStorage.setItem(JSON.stringify(thingsYouNeedToDo));
+}
+
+function addThingsYouNeedToDoToStorage(thingsYouNeedToDo){
+    const listItem = document.createElement("List item");
+    listItem.setAttribute("data-id", thingsYouNeedToDo.id);
+}
